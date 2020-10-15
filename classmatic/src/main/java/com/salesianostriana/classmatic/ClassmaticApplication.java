dbd655ebc5df3f69489147e2250eed21af50458a
Carlos <cgl76490@gmail.com>
2020-10-15 19:08:54 +0200

    Add de la estructura del proyecto

---
+++ classmatic/src/main/java/com/salesianostriana/classmatic/ClassmaticApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.classmatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmaticApplication.class, args);
+	}
+
+}
