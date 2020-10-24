701a02d8e56ba39ae9588b00c3767c156e757cd1
Carlos <cgl76490@gmail.com>
2020-10-24 22:18:59 +0200

    Sin avance

--- classmatic/src/main/java/com/salesianostriana/classmatic/entidades/Titulo.java
+++ classmatic/src/main/java/com/salesianostriana/classmatic/entidades/Titulo.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
