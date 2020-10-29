dc514db6dfb1f8dad9dfc8059fb81a6b28786a99
Carlos <cgl76490@gmail.com>
2020-10-29 11:20:04 +0100

    Ya sube archivos

--- classmatic/src/main/java/com/salesianostriana/classmatic/controladores/AlumnoController.java
+++ classmatic/src/main/java/com/salesianostriana/classmatic/controladores/AlumnoController.java
@@ -1,14 +1,32 @@
 package com.salesianostriana.classmatic.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import com.salesianostriana.classmatic.entidades.Alumno;
 import com.salesianostriana.classmatic.entidades.Asignatura;
 import com.salesianostriana.classmatic.entidades.SituacionExcepcional;
+import com.salesianostriana.classmatic.ficheros.StorageService;
 import com.salesianostriana.classmatic.servicios.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +47,8 @@ public class AlumnoController {
     @Autowired
     CursoServicio cursoServicio;
 
-    /*@Autowired
-    StorageService storageService;*/
+    @Autowired
+    StorageService storageService;
 
     @Autowired
     SituacionExcepcionalServicio situacionExcepcionalServicio;
@@ -57,43 +75,6 @@ public class AlumnoController {
         return "alumno/alumnoConvalidaciones";
     }
 
-/*
-    @GetMapping("/convalidacionde/{id}")
-    public String convalidarAsignatura(@PathVariable Long id, Model model, @AuthenticationPrincipal Alumno alumno){
-        //SituacionExcepcional sit=new SituacionExcepcional();
-        *//*sit.setAsignatura(asignaturaServicio.findById(id));
-        sit.setFechaSolicitud(LocalDate.now());
-        sit.setResuelta(false);
-        sit.setAlumno(alumno);
-        sit.setFechaResolucion(null);*//*
-        *//*System.out.println("Ha entrado en la peticion de la convalidacion de una asignatura..................................................");
-        model.addAttribute("convalidacion", new SituacionExcepcional());
-        System.out.println("Ha anyadido la SituacionExcepcional al model..................................................");
-        model.addAttribute("asignatura",asignaturaServicio.findById(id));
-        System.out.println("Ha anyadido la asignatura  al model..................................................");*//*
-        return "alumno/alumnoConvalidar";
-    }
-
-
-    @PostMapping("/convalidacionde/{id}")
-    public String convalidar(*//*@RequestParam("archivo") MultipartFile archivo,*//* @PathVariable Long id,
-                             @ModelAttribute("convalidacion")SituacionExcepcional situacionExcepcional,
-                             @AuthenticationPrincipal Alumno alumno,
-                             Model model){
-        *//*if(archivo.isEmpty()){
-            return convalidarAsignatura( id,  model,  alumno);
-        }else{
-            //implementar tratamiento de fichero
-            String rutaArchivo = storageService.store(archivo, situacionExcepcional.getId());
-            situacionExcepcional.setArchivoConvalidacion(MvcUriComponentsBuilder
-                .fromMethodName(AlumnoController.class, "serverFile", rutaArchivo)
-                .build().toUriString());
-            situacionExcepcionalServicio.edit(situacionExcepcional);
-            return accederConvalidaciones( alumno,  model);
-        }*//*
-        //alumnoServicio.solicitarConvalidacion(id, situacionExcepcional, situacionExcepcionalServicio);
-        return accederConvalidaciones( alumno,  model);
-    }*/
 
     @GetMapping("/convalidarAsignatura/{id}")
     public String convalidarAsignatura(@PathVariable Long id, Model model){
@@ -102,10 +83,33 @@ public class AlumnoController {
         return "alumno/convalidarAsignatura";
     }
 
-    @PostMapping()
-    public String enviarConvalidacion(){
-        return "";
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().body(file);
     }
+
+    @PostMapping("/convalidarAsignatura/{id}")
+    public String enviarConvalidacion(@ModelAttribute("situacionExcepcional")SituacionExcepcional situacionExcepcional,
+                                      @PathVariable Long id, Model model,
+                                      @RequestParam("archivo")MultipartFile archivo,
+                                      BindingResult bindingResult, @AuthenticationPrincipal Alumno alumno){
+        System.out.println(situacionExcepcional.getId()+"----------------------------------------------------------");
+        if(!archivo.isEmpty()){
+            String ruta = storageService.store(archivo, situacionExcepcional.getId());
+            situacionExcepcional.setArchivoConvalidacion(MvcUriComponentsBuilder
+                .fromMethodName(AlumnoController.class, "serveFile", ruta).build().toUriString());
+            alumnoServicio.convalidar(asignaturaServicio, situacionExcepcionalServicio, id, situacionExcepcional,
+                    alumno, ruta );
+            return accederConvalidaciones( alumno, model);
+        }else{
+            return convalidarAsignatura( id,  model);
+        }
+
+    }
+
+
 
 
     @GetMapping("/ampliaciones")
--- classmatic/src/main/java/com/salesianostriana/classmatic/servicios/AlumnoServicio.java
+++ classmatic/src/main/java/com/salesianostriana/classmatic/servicios/AlumnoServicio.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -107,6 +108,30 @@ public class AlumnoServicio extends ServicioBaseImp<Alumno, Long, AlumnoReposito
         al.addSituacionExcepcional(situacionExcepcional);
         edit(al);
         situacionExcepcionalServicio.edit(situacionExcepcional);
+    }
+
+    public void convalidar(AsignaturaServicio asignaturaServicio, SituacionExcepcionalServicio situacionExcepcionalServicio,
+                           Long idAsignatura, SituacionExcepcional situacionExcepcional, Alumno alumno,
+                           String ruta){
+        System.out.println(idAsignatura+"------------------------------------------------------ID Asignatura");
+        Asignatura as=asignaturaServicio.findById(idAsignatura);
+        Alumno al=findById(alumno.getId());
+        SituacionExcepcional sit = new SituacionExcepcional();
+        sit.setArchivoConvalidacion(ruta);
+        sit.setResuelta(false);
+        sit.setFechaSolicitud(LocalDate.now());
+        situacionExcepcionalServicio.save(sit);
+        //situacionExcepcional.setArchivoConvalidacion(ruta);
+        //situacionExcepcional.setResuelta(false);
+        //situacionExcepcional.setFechaSolicitud(LocalDate.now());
+        //situacionExcepcionalServicio.save(situacionExcepcional);
+        al.addSituacionExcepcional(sit);
+        //System.out.println(situacionExcepcional.getId()+"------------------------------------------------------ID Convalidacion");
+        System.out.println(sit.getId()+"------------------------------------------------------ID Convalidacion");
+        as.addSituacionExcepcional(sit);
+        edit(al);
+        asignaturaServicio.edit(as);
+        situacionExcepcionalServicio.edit(sit);
     }
 
 
